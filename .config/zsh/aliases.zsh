8149e4c74b8ba41cbb2ea152ceea90474fccb078
Alex <ahjorth@gmail.com>
2021-09-12 12:04:25 +0200

    updates

---
+++ .config/zsh/aliases.zsh
@@ -0,0 +1,9 @@
+alias dotfiles="git --work-tree=$HOME/ --git-dir=$HOME/.local/share/dotfiles.git"
+
+alias l='ls -lh --group-directories-first'
+alias ll='ls -lAh --group-directories-first'
+alias rm='rm -i'
+
+alias adu='sudo apt update && sudo apt dist-upgrade'
+alias aac='sudo apt autoclean'
+alias aar='sudo apt autoremove'
--- bin/setupenv.sh
+++ bin/setupenv.sh
@@ -33,6 +33,27 @@ install_powerline_fonts() {
 	sudo rm -rf powerline-fonts
 }
 
+install_oh-my-zsh () {
+    if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
+        if [[ ! -d ~/.oh-my-zsh/ ]]; then
+            echo "Cloning robbyrussell/oh-my-zsh"
+            git clone --depth=1 http://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+        else
+            echo "Oh-My-Zsh is already installed."
+        fi
+
+        ask "Do you want to make Zsh your default shell?"
+        if [[ "$REPLY" =~ ^[Yy]$  ]]; then
+            chsh -s $(which zsh)
+        fi
+    else
+        echo -e "\nZsh is not installed. Installing ..."
+        sudo apt-get install zsh
+        install_oh-my-zsh
+    fi
+}
+
 install_packages
 setup_local_gitconfig
-#install_powerline_fonts
+install_oh-my-zsh
+install_powerline_fonts
